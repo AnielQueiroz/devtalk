7383460f6643eb7453d6bd6df660187226551cf8
Aniel <anielqsilva@gmail.com>
2024-12-18 18:46:21 -0300

    feat: creating navbar

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useAuthStore } from "../store/useAuthStore";
+import { LogOut, MessagesSquare, Settings, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	const { authUser, logout } = useAuthStore();
 
-export default Navbar
+	return (
+		<div className="navbar bg-base-100">
+			<div className="flex-1">
+        <div className="flex gap-2 items-center">
+          <MessagesSquare className="size-6" />
+          <a href="/" className="text-xl font-bold">talkYou</a>
+        </div>
+			</div>
+			{authUser && (
+				<div className="flex-none gap-2">
+					<div className="form-control">
+						<input
+							type="text"
+							placeholder="Buscar..."
+							className="input input-bordered w-24 md:w-auto"
+						/>
+					</div>
+					<div className="dropdown dropdown-end">
+						<button
+							tabIndex={0}
+							type="button"
+							className="btn btn-ghost btn-circle avatar"
+						>
+							<div className="w-10 rounded-full">
+								<img
+									alt="Foto de perfil"
+									src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+								/>
+							</div>
+						</button>
+						<ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow space-y-2">
+							<li className="flex items-center gap-2 transition-colors pointer-events-none">
+								{/* <span className="hidden sm:inline">Menu</span> */}
+							</li>
+							<Link className="flex items-center gap-2 transition-colors-hover hover:text-primary" to={"/profile"}>
+                <User className="size-5" />
+								<span className="hidden sm:inline">Perfil</span>
+							</Link>
+							<Link className="flex items-center gap-2 transition-colors-hover hover:text-primary" to={"/settings"}>
+                <Settings className="size-5" />
+								<span className="hidden sm:inline">Configurações</span>
+							</Link>
+							<button className="flex items-center gap-2 transition-colors-hover hover:text-primary" type="button" onClick={logout}>
+                <LogOut className="size-5" />
+								<span className="hidden sm:inline">Sair</span>
+							</button>
+						</ul>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Navbar;
