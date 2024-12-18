486e67a9008a2cfa6b9b0a24c8bfc0d6c18da4b5
Aniel <anielqsilva@gmail.com>
2024-12-18 18:47:16 -0300

    chore: protected route

---
+++ front/src/utils/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import type React from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ element }: { element: React.ReactNode }) => {
+	const { authUser } = useAuthStore();
+
+	if (!authUser) return element;
+	
+	console.log(element);
+	return element;
+};
+
+export default ProtectedRoute;
