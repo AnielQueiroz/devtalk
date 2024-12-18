1e411d4eb2b7dddf27df95d50d0e6e6e47a246ed
Aniel <anielqsilva@gmail.com>
2024-12-18 18:45:47 -0300

    feat: creating logo

---
+++ front/src/components/Logo.tsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+	return (
+		<div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+			<div className="size-6 text-primary" />
+			<h2 className="text-3xl font-bold">talkYou</h2>
+		</div>
+	);
+};
+
+export default Logo;
