56f533246218d9ad4a99a60459cc76e20b4fb524
Aniel <anielqsilva@gmail.com>
2024-12-18 13:12:58 -0300

    feat: implementing signup page

--- front/src/store/useAuthStore.ts
+++ front/src/store/useAuthStore.ts
@@ -1,50 +1,60 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+import { AxiosError } from "axios";
 
 interface AuthState {
-    authUser: null;
-    isSigningUp: boolean;
-    isLoggingIn: boolean;
-    isUpdatingProfile: boolean;
-    isCheckingAuth: boolean;
-    checkAuth: () => Promise<void>;
-    signUp: (data: Data) => Promise<void>;
+	authUser: null;
+	isSigningUp: boolean;
+	isLoggingIn: boolean;
+	isUpdatingProfile: boolean;
+	isCheckingAuth: boolean;
+	checkAuth: () => Promise<void>;
+	signUp: (data: Data) => Promise<void>;
 }
 
 interface Data {
-    email: string;
-    fullName: string;
-    password: string
+	email: string;
+	fullName: string;
+	password: string;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-    authUser: null,
-    isSigningUp: false,
-    isLoggingIn: false,
-    isUpdatingProfile: false,
-    
-    isCheckingAuth: true,
+	authUser: null,
+	isSigningUp: false,
+	isLoggingIn: false,
+	isUpdatingProfile: false,
 
-    checkAuth: async() => {
-        try {
-            const res = await axiosInstance.get("/auth/check");
+	isCheckingAuth: true,
 
-            console.log(res.data);
+	checkAuth: async () => {
+		try {
+			const res = await axiosInstance.get("/auth/check");
 
-            set({ authUser: res.data});
-        } catch (error) {
-            set({ authUser: null });
-            console.log("Erro ao verificar autenticação: ", error);
-        } finally {
-            set({ isCheckingAuth: false });
-        }
-    },
+			console.log(res.data);
 
-    signUp: async(data) => {
-        try {
-            console.log(data);
-        } catch (error) {
-            console.log("Erro ao fazer cadastro: ", error);
-        }
-    }
+			set({ authUser: res.data });
+		} catch (error) {
+			set({ authUser: null });
+			console.log("Erro ao verificar autenticação: ", error);
+		} finally {
+			set({ isCheckingAuth: false });
+		}
+	},
+
+	signUp: async (data): Promise<void> => {
+		set({ isSigningUp: true });
+		try {
+			const res = await axiosInstance.post("/auth/signup", data);
+			set({ authUser: res.data });
+			toast.success("Conta criada com sucesso!");
+		} catch (error: unknown) {
+			if (error instanceof AxiosError) {
+				toast.error(error.response?.data.message);
+			}
+			console.log("Erro ao fazer cadastro: ", error);
+		} finally {
+			set({ isSigningUp: false });
+		}
+	},
 }));
