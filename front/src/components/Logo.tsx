06c5ab98ddbcf0a35180f31660b86a38e18f2abe
Aniel <anielqsilva@gmail.com>
2024-12-19 11:12:03 -0300

    chore: changing the name to devTalk

--- front/src/components/Logo.tsx
+++ front/src/components/Logo.tsx
@@ -2,7 +2,7 @@ const Logo = () => {
 	return (
 		<div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
 			<div className="size-6 text-primary" />
-			<h2 className="text-3xl font-bold">talkYou</h2>
+			<h2 className="text-3xl font-bold">devTalk</h2>
 		</div>
 	);
 };
