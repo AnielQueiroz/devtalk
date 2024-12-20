baedbe9e6cdd8c3e8b703da219537db747c0ccc2
Aniel <anielqsilva@gmail.com>
2024-12-20 09:15:53 -0300

    chore: creating store to language

--- front/src/store/useThemeStore.ts
+++ front/src/store/useThemeStore.ts
@@ -2,13 +2,20 @@ import { create } from "zustand";
 
 interface ThemeStore {
     theme: string;
+    language: string
     setTheme: (theme: string) => void
+    setLanguage: (language: string) => void
 }
 
 export const useThemeStore = create<ThemeStore>((set) => ({
     theme: localStorage.getItem("chat-them") || "synthwave",
+    language: localStorage.getItem("devtalk-lang") || "pt",
     setTheme: (theme: string) => {
         localStorage.setItem("chat-them", theme);
         set({ theme})
-    } 
+    } ,
+    setLanguage: (language: string) => {
+        localStorage.setItem("devtalk-lang", language);
+        set({ language })
+    }
 }));
