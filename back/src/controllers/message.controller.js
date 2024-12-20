c4862daceb0a1ff813b8f4f28bbb50d6669ce679
Aniel <anielqsilva@gmail.com>
2024-12-20 17:29:34 -0300

    fix: corrigir resposta da rota de usuários filtrados na barra lateral

--- back/src/controllers/message.controller.js
+++ back/src/controllers/message.controller.js
@@ -7,7 +7,7 @@ export const getUsersForSideBar = async (req, res) => {
         const loggedInUserId = req.user._id;
         const filteredUsers = await User.find({_id: { $ne: loggedInUserId }}).select("-password");
 
-        return res.status(200).json({ filteredUsers });
+        return res.status(200).json( filteredUsers );
     } catch (error) {
         console.log("Erro ao obter usuarios para barra lateral: ", error);
         return res.status(500).json({ message: "Erro ao obter usuarios para barra lateral" });
