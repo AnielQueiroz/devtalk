6b9cd383d7cf6fc0dc4582018a641053311ebce2
AnielQueiroz <anielqsilva@gmail.com>
2024-12-19 23:37:48 -0300

    style: adding new themes

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,29 +1,12 @@
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 
 export default {
-
-  content: [
-
-    "./index.html",
-
-    "./src/**/*.{js,ts,jsx,tsx}",
-
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
-
     extend: {},
-
-  },
-
-  daisyui: {
-    themes: [
-      "synthwave",
-      "black",
-      "retro"
-    ]
   },
 
   // daisyui: {
@@ -38,5 +21,27 @@ export default {
   // },
 
   plugins: [daisyui],
-
-}
+  daisyui: {
+    themes: [
+      "synthwave",
+      "black",
+      "light",
+      "retro",
+      {
+        devTalk: {
+          primary: "#000000",
+          secondary: "#ffff00",
+          accent: "#ff00ff",
+          neutral: "#ff00ff",
+          "base-100": "#000000",
+          "base-200": "#333300",
+          // "base-300": "#330033",
+          info: "#00ffff",
+          success: "#00ff00",
+          warning: "#00ff00",
+          error: "#ff0000",
+        },
+      },
+    ],
+  },
+};
