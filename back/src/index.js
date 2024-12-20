c16785f3be0da7d6dcb9a254a710a4b4fc71c0c9
AnielQueiroz <anielqsilva@gmail.com>
2024-12-19 22:38:08 -0300

    chore: configuring size for profile pic

--- back/src/index.js
+++ back/src/index.js
@@ -13,12 +13,17 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(
+  express.urlencoded({ limit: "5mb", extended: true, parameterLimit: 50000 })
+);
 app.use(cookieParser());
-app.use(cors({ 
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
