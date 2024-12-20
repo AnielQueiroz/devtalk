76a111829b5bbf0c3feb4da5580786692bca66c7
AnielQueiroz <anielqsilva@gmail.com>
2024-12-20 01:22:28 -0300

    styles: some styles

--- front/src/pages/ProfilePage.tsx
+++ front/src/pages/ProfilePage.tsx
@@ -30,7 +30,7 @@ const ProfilePage = () => {
 	};
 
 	return (
-		<div className="pt-28 sm:pt-16">
+		<div className="min-h-screen pt-28 sm:pt-16">
 			<div className="max-w-2xl mx-auto p-4 py-6">
 				<div className="bg-base-300 rounded-xl p-6 space-y-8">
 					<div className="text-center">
