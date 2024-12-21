5e4ae9dadb21b4fd7ac2092ac2f93a360912be9e
AnielQueiroz <anielqsilva@gmail.com>
2024-12-21 17:38:52 -0300

    feat: add CommunityContainer component for displaying selected community

---
+++ front/src/components/CommunityContainer.tsx
@@ -0,0 +1,9 @@
+const CommunityContainer = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1 className="text-2xl font-bold">Comunidade Selecionada</h1>
+    </div>
+  );
+};
+
+export default CommunityContainer;
