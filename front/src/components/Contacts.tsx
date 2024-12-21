aa8fed714ee26eca26c864cf3de9b62e25e88df4
AnielQueiroz <anielqsilva@gmail.com>
2024-12-21 17:39:11 -0300

    feat: add Contacts component for displaying and selecting users

---
+++ front/src/components/Contacts.tsx
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import SidebarSkeleton from "./skeletons/SidebarSkeleton";
+import { Users } from "lucide-react";
+import { t } from "i18next";
+
+const Contacts = () => {
+  const { getUsers, users, selectedUser, setSelectedUser, setSelectedCommunity, isUsersLoading } =
+    useChatStore();
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  const onlineUsers: string | string[] = [];
+
+  return (
+    <>
+      {isUsersLoading ? (
+        <SidebarSkeleton />
+      ) : (
+        <div className="overflow-y-auto w-full py-3">
+            {/* titulo */}
+            <div className="flex items-center justify-center gap-2 p-3 border-b border-base-300">
+                <Users className="size-6" />
+                <h1 className="text-lg font-bold">{t('contacts')}</h1>
+            </div>
+          {users.map((user) => (
+            <button
+              type="button"
+              key={user._id}
+              onClick={() => {
+                setSelectedUser(user)
+                setSelectedCommunity(null)
+              }}
+              className={`w-full p-3 flex items-center gap-3 hover:bg-base-300 transition-colors ${
+                selectedUser?._id === user._id
+                  ? "bg-base-300 ring-1 ring-base-300"
+                  : ""
+              }`}
+            >
+              <div className="relative mx-auto lg:mx-0">
+                <img
+                  src={user.profilePic || "/avatar.png"}
+                  alt={user.fullName}
+                  className="rounded-full size-12 object-cover"
+                />
+                {onlineUsers.includes(user._id) && (
+                  <span className="absolute bottom-0 right-0 size-3 bg-green-500 rounded-full ring-2 ring-zinc-900" />
+                )}
+              </div>
+
+              <div className="hidden lg:block text-left min-w-0">
+                <div className="font-medium truncate">{user.fullName}</div>
+                <div className="text-sm text-zinc-400">
+                  {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+                </div>
+              </div>
+            </button>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Contacts;
