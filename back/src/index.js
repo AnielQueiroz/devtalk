29d2b3eb2dd1195afb2210c504f3127a78cb6eea
AnielQueiroz <anielqsilva@gmail.com>
2024-12-23 19:43:54 -0300

    style: serve static files in production and handle fallback to index.html

--- back/src/index.js
+++ back/src/index.js
@@ -6,12 +6,15 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
+import path from "node:path";
+
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(
@@ -27,6 +30,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../front/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front/dist/index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
