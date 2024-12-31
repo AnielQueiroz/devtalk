a340c57c5229647f3822d0eee4b43381ed702108
Aniel <anielqsilva@gmail.com>
2024-12-31 10:19:36 -0300

    feat: adicionar rotas para contagem de mensagens não lidas e marcar mensagens como lidas

--- back/src/routes/message.route.js
+++ back/src/routes/message.route.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSideBar, sendMessage, getInteractedContacts } from "../controllers/message.controller.js";
+import { getMessages, getUsersForSideBar, sendMessage, getInteractedContacts, markMessageAsRead, getUnreadMessagesCounts } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSideBar);
 router.get("/interacted-contacts", protectRoute, getInteractedContacts);
+router.get("/unread-messages-count", protectRoute, getUnreadMessagesCounts);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.put("/mark-as-read/:senderId", protectRoute, markMessageAsRead);
 
 export default router;
